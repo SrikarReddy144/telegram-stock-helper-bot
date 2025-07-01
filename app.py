59bcc30fb87fb0d97d39d96f36277475635c7544
SrikarReddy144 <155365096+SrikarReddy144@users.noreply.github.com>
2025-07-02 00:36:45 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request
-import bot
+import telegram
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-@app.route('/SECRET_PATH', methods=['POST'])
+@app.route(f"/{os.getenv('SECRET_PATH')}", methods=["POST"])
 def webhook():
-    update = request.get_json()
-    bot.handle_update(update)
-    return "", 200
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    return "ok"
+
+# Optional for local testing
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
